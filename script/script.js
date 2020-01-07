b7ece991aba9173d16e5e2eabbec47f0de4af517
YuhaoZhang-tirsky <tirsky@outlook.com>
2020-01-07 14:54:28 +1100

    home page computer view done

--- script/script.js
+++ script/script.js
@@ -1,6 +1,15 @@
+/**check if the page has been resize*/
+window.onresize = function(e) {
+  var width = window.innerWidth;
+  if (width > 991) {
+    document.getElementById("mobile-menu").style.display = "block";
+  } else {
+    document.getElementById("mobile-menu").style.display = "none";
+  }
+}
+/**change menu visibility in mobile view. */
 showMenu = function () {
   var menuStatus = document.getElementById("mobile-menu").style.display;
-  console.log(menuStatus);
   if (menuStatus == "block") {
     document.getElementById("mobile-menu").style.display = "none";
   }
@@ -8,3 +17,4 @@ showMenu = function () {
     document.getElementById("mobile-menu").style.display = "block";
   }
 }
+
